04dbe998ed239e58634995946e18de0ac5268822
AEren10 <ahmet28eren@gmail.com>
2026-09-16 02:52:09 +0300

    ANT fix: TrialDetailHero karakter kodlama hatasi duzeltildi

--- src/screens/trial/components/TrialDetailHero.js
+++ src/screens/trial/components/TrialDetailHero.js
@@ -1,4 +1,4 @@
-import { View, Text, StyleSheet } from "react-native";
+﻿import { View, Text, StyleSheet } from "react-native";
 import Animated, { FadeInDown } from "react-native-reanimated";
 
 import { Icon, StatBlock, Chip } from "../../../components/design";
