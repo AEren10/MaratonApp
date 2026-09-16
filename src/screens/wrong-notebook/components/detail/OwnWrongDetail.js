56d1a8d240f3f05073b7e55a2fd62396199d8156
AEren10 <ahmet28eren@gmail.com>
2026-09-16 03:01:42 +0300

    ANT fix: OwnWrongDetail karakter kodlamalari

--- src/screens/wrong-notebook/components/detail/OwnWrongDetail.js
+++ src/screens/wrong-notebook/components/detail/OwnWrongDetail.js
@@ -1,4 +1,4 @@
-import { View, Text, ScrollView, Pressable, StyleSheet } from "react-native";
+﻿import { View, Text, ScrollView, Pressable, StyleSheet } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { useNavigation, useRoute } from "@react-navigation/native";
 import Animated, { FadeInDown } from "react-native-reanimated";
@@ -109,3 +109,4 @@ const styles = StyleSheet.create({
     justifyContent: "center",
   },
 });
+
